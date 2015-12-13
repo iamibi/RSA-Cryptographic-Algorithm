0801607abdb51d8779d4fd5c0926a8dcf51f6763
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-12-13 21:33:56 +0530

    Generate large prime numbers

---
+++ GeneratePrime.py
@@ -0,0 +1,84 @@
+import random
+
+
+'''
+    Miller-Rabin Primality test.
+
+    Code Borrowed from: http://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Python
+'''
+
+def _try_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
+        return False
+    for i in range(s):
+        if pow(a, 2**i * d, n) == n-1:
+            return False
+    return True # n  is definitely composite
+ 
+def is_prime(n, _precision_for_huge_n=16):
+    if n in _known_primes or n in (0, 1):
+        return True
+    if any((n % p) == 0 for p in _known_primes):
+        return False
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+    if n < 1373653: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+    if n < 25326001: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+    if n < 118670087467: 
+        if n == 3215031751: 
+            return False
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+    if n < 2152302898747: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+    if n < 3474749660383: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+    if n < 341550071728321: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+    # otherwise
+    return not any(_try_composite(a, d, n, s) 
+                   for a in _known_primes[:_precision_for_huge_n])
+
+_known_primes = [2, 3]
+_known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
+
+
+'''
+    First check whether the number is in the given list of prime numbers less than 1000, as it will
+    be easy. If its not, then check if it is divisible by any of the given smaller prime numbers.
+'''
+
+def checkPrime(x):
+    if x < 2:
+        return False
+
+    #Smaller Prime numbers i.e., less than 1000
+    smaller_prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+
+    #if the given number is in smaller primes, then the number is prime
+    if x in smaller_prime:
+        return True
+
+    #if it is not in smaller prime, check whether the number is divisible
+    #by any of the number within the smaller prime
+    for i in smaller_prime:
+
+        #if the number is divisible, then it is not prime
+        if x % i == 0:
+            return False
+
+    #check the number through Miller-Rabin primality test
+    return is_prime(x)
+
+'''
+    Generate large primes of the order of 1024-bits
+'''
+
+def generateLargePrime(keySize=1024):
+    while (1):
+        num = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
+        if checkPrime(num):
+            return num
