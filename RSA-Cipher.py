cd4bc964acbff354c23e5ee1b38242b41dc53c03
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-12-17 21:25:13 +0530

    RSA Cipher

--- RSA-Cipher.py
+++ RSA-Cipher.py
@@ -4,27 +4,36 @@
 '''
 
 #import the necessary files
-import sys, os, EncryptMessage, DecryptMessage
-
-flag = 0                                                        #set the flag for digital signature
+import sys, os, pickle, EncryptMessage, DecryptMessage
 
 def main():
     print ("RSA ASYMMETRIC CRYPTOGRAPHIC ALGOROTHM")
     mode = input("Enter the mode (Encrypt or Decrypt): ")       #Choose between encrypt or decrypt
-    filename = 'encrypted_file.txt'                             #encrypted message file name
-    key_file = ''
+    filename = 'encrypted_file.pickle'                                 #encrypted message file name
     
+    try:
+        with open('key.pickle', 'rb') as keyCheck:
+            KEY = pickle.load(keyCheck)
+    except pickle.PickleError as pick:
+        print ("Key File Error: " + str(pick))
+        sys.exit()
+    
+    KEY = KEY.split(',')
+    print (KEY)
     #if the user want to encrypt a message
     if mode[0] in 'E e'.split():
-        key_file = "key_file_pubkey.txt"
+        dg = input("Do you want to digitally sign the message? [Yes/No]: ")
+        if dg[0] in 'Y y'.split():
+            key_file = KEY[1]
+        elif dg[0] in 'N n'.split():
+            key_file = KEY[0]
+        else:
+            sys.exit("Invalid entry...Exiting")
         if not (os.path.exists(key_file)):
             key_file = input("Enter the public key file name with extension: ")
             
             if not (os.path.exists(key_file)):
                 sys.exit("The file %s doesn't exist. First run keyGenerate.py..."%(key_file))
-                
-            #check whether the user want to digitally sign the message
-            CheckKeyFile('Encrypt', key_file, 'Kv', 'Private Key')
         
         #Enter the message and write it to a file
         message = input("Enter the message that you want to encrypt: ")
@@ -34,42 +43,33 @@ def main():
         
     #else if the user want to decrypt the message
     elif mode[0] in 'D d'.split():
-        key_file = "key_file_privkey.txt"
+        dg = input("Is your message digitally signed? [Yes/No]: ")
+        if dg[0] in 'Y y'.split():
+            flag_k = 1
+        elif dg[0] in 'N n'.split():
+            flag_k = 0
+        else:
+            sys.exit("Invalid Entry...Exiting")
+
+        if flag_k == 1:
+            key_file = KEY[0]
+        elif flag_k == 0:
+            key_file = KEY[1]
+        print (key_file)
         if not (os.path.exists(filename)):
             sys.exit("The encrypted message file %s doesn't exist..."%(filename))
 
-        if (not (os.path.exists(key_file)) or flag):
+        if not (os.path.exists(key_file)):
             key_file = input("Enter the private key file name with extension: ")
             if not (os.path.exists(key_file)):
                 sys.exit("The file %s doesn't exist. First run keyGenerate.py..."%(key_file))
-        
-            #if the file was digitally signed
-            CheckKeyFile('Decrypt', key_file, 'Kp', 'Public Key')
 
-        print ("Decrypting and Writing to the file decrypted_file.txt")
+        print ("Decrypting and Writing to the file decrypted_file.pickle")
         
         decrypted_message = DecryptMessage.Decrypt(filename, key_file)
         print ("Decrypted message: %s"%(decrypted_message))
     else:
-        print ("Wrong Choice...Exiting")
-        sys.exit()
-
-def CheckKeyFile(mode, key_file, key, key_type):
-    flag = 1
-    try:
-        with open (key_file, 'r') as fo:
-            content = fo.read()
-            if key in content:
-                print ("Are you sure you want to %s the message using %s(Digital Signature) [Yes/No]: "%(mode, key_type), end = '')
-                ch = input()
-                if ch in "Yes yes y Y".split():
-                    print ("Digital Signature.....[Selected]")
-                elif ch in "No no n N".split():
-                    sys.exit ("Re-run the program with correct file name")
-                else:
-                    sys.exit ("Invalid Choice...Exiting")
-    except IOError as er:
-        print ("File Error: " + str(er))
+        print ("Invalid Choice...Exiting")
         sys.exit()
 
 if __name__ == '__main__':
